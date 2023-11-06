2afaa54fb63486862fae488c3fac379228919e08
rdxkeerthi <sambavam102@gmail.com>
2023-11-06 19:28:15 +0530

    rdx

--- webserver.py
+++ webserver.py
@@ -1,33 +1,3 @@
-#  ex01 Developing a Simple Webserver
-
-# AIM:
-
-To develop a simple webserver to serve html programming pages.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-HTML content creation is done
-
-### Step 2:
-
-Design of webserver workflow
-
-### Step 3:
-
-Implementation using Python code
-
-### Step 4:
-
-Serving the HTML pages.
-
-### Step 5:
-
-Testing the webserver
-
-## PROGRAM:
-'''
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content='''
@@ -57,9 +27,3 @@ print("This is my webserver")
 server_address =('keerthi',2323)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-'''
-## OUTPUT:
-![OUTPUT](./web1.png)
-![OUTPUT](./web2.png)
-## RESULT:
-The program is executed succesfully
